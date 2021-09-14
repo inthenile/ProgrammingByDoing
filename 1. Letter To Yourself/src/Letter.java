6eea1e0a70b184908d8f7df5fcff26fe41f44041
S.Ege <joyceismyhomeboi@gmail.com>
2021-09-14 19:10:55 +0200

    first assignment

---
+++ 1. Letter To Yourself/src/Letter.java
@@ -0,0 +1,18 @@
+public class Letter {
+    public static void main(String[] args) {
+        System.out.print("+");
+        System.out.print("----------------------------------------------------------------");
+        System.out.println("+");
+        System.out.println("|                                                        ####    |");
+        System.out.println("|                                                        ####    |");
+        System.out.println("|                                                        ####    |");
+        System.out.println("|                                                                |");
+        System.out.println("|                                    Salim Ege                   |");
+        System.out.println("|                                    2 Metrica Province          |");
+        System.out.println("|                                    Tyria, Rata Sum             |");
+        System.out.println("|                                                                |");
+        System.out.print("+");
+        System.out.print("----------------------------------------------------------------");
+        System.out.println("+");
+    }
+}
