d61c05e6da79279ff6b8c1ed750920d5f8f561e5
S.Ege <joyceismyhomeboi@gmail.com>
2021-09-22 18:02:55 +0200

    76th assignment: blackjack project

--- 76. Project - Blackjack/src/Main.java
+++ 76. Project - Blackjack/src/Main.java
@@ -10,33 +10,43 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     Random rand = new Random();
     scanner.nextLine();
+    //Making an array of suits to grab using a random number
+    String [] suits = {"spades", "hearts", "diamonds", "clubs"};
     // Information about the player's hand
     int playerCard1 = rand.nextInt(10)+2; //In order to get a card between 2-11
     int playerCard2 = rand.nextInt(10)+2;
     int playerTotal = playerCard1 + playerCard2;
-    System.out.println("You get a " + playerCard1 + " and a " + playerCard2 + ".");
+
+    //Selecting different suits for different cards, so they are not the same
+    int playerSuit1 = rand.nextInt(3);
+    int playerSuit2 = rand.nextInt(3);
+
+    System.out.println("You get " + playerCard1 + " of " + suits[playerSuit1] + " and " + playerCard2 + " of " + suits[playerSuit2] + ".");
     Thread.sleep(1000);
-    System.out.println("Your total is " + playerTotal + "\n");
+    System.out.println("Your total is " + playerTotal + ".\n");
     // Information about the computer's hand
     int dealerCard1 = rand.nextInt(10) + 2;
     int dealerCard2 = rand.nextInt(10) + 2;
     int dealerTotal = dealerCard1 + dealerCard2;
+    int dealerSuit1 = rand.nextInt(3);
+    int dealerSuit2 = rand.nextInt(3);
     //Using Thread.sleep to create a sense of excitement instead of reflecting a huge amount of text all at once.
     Thread.sleep(1000);
-    System.out.println("The dealer has a " + dealerCard1 + " and a hidden card.");
+    System.out.println("The dealer has " + dealerCard1 + " of " + suits[dealerSuit1] + " and a hidden card.");
     Thread.sleep(1000);
     System.out.println("His total is hidden too.\n");
 
     try{
         //21 is the total number that, by default, ends the game.
-        while(playerTotal < 21) {
+        while(playerTotal <= 21) {
             //Asking the player what they want to do
             System.out.println("Would you like to \"hit\" or \"stay\"?");
             String prompt = scanner.nextLine();
             if (prompt.equals("hit") || prompt.equals("HIT")) {
                 int newPlayerCard = rand.nextInt(10) + 2;
+                int newPlayerSuit = rand.nextInt(3);
                 Thread.sleep(1000);
-                System.out.println("You drew a " + newPlayerCard + ".");
+                System.out.println("You drew " + newPlayerCard + " of " + suits[newPlayerSuit] + ".");
                 playerTotal = playerTotal + newPlayerCard;
                 Thread.sleep(1000);
                 System.out.println("Your new total is " + playerTotal + ".\n");
@@ -45,17 +55,18 @@ public class Main {
                 Thread.sleep(1000);
                 System.out.println("It's the dealer's turn.");
                 Thread.sleep(1000);
-                System.out.println("His hidden card was a " + dealerCard2 + ".");
+                System.out.println("His hidden card was " + dealerCard2 + " of " + suits[dealerSuit2] + ".");
                 Thread.sleep(1000);
-                System.out.println("His total was " + dealerTotal + ".");
+                System.out.println("His total was " + dealerTotal + ".\n");
                 //Dealer's moves
-                //While below 16, he hits
+                //While 16, or below 16, he hits
                 while(dealerTotal <= 16){
                     int newDealerCard = rand.nextInt(10) +2;
+                    int newDealerSuit = rand.nextInt(3);
                     Thread.sleep(1000);
-                    System.out.println("Dealer chooses to hit");
+                    System.out.println("Dealer chooses to hit.");
                     Thread.sleep(1000);
-                    System.out.println("He draws a " + newDealerCard + ".");
+                    System.out.println("He draws " + newDealerCard + " of " + suits[newDealerSuit] + ".");
                     dealerTotal = dealerTotal + newDealerCard;
                     Thread.sleep(1000);
                     System.out.println("His new total is " + dealerTotal + ".\n");
@@ -65,20 +76,27 @@ public class Main {
                 Thread.sleep(1000);
                 System.out.println("The dealer's total is " + dealerTotal);
                 Thread.sleep(1000);
-                System.out.println("Your total is " + playerTotal);
+                System.out.println("Your total is " + playerTotal + ".\n");
 
                 //Announcing the winner
-                if (playerTotal > dealerTotal){
+                if(dealerTotal > 21){
+                    Thread.sleep(1000);
+                    System.out.println("The dealer's hand totals more than 21. He loses.");
+                    playerWin();
+                } else if (playerTotal > dealerTotal){
+                    Thread.sleep(1000);
                     playerWin();
                 } else if (playerTotal == dealerTotal) {
+                    Thread.sleep(1000);
                     System.out.println("Tie means a win for the dealer!");
                     dealerWin();
                 } else {
+                    Thread.sleep(1000);
                     dealerWin();
                 }
             }
         }
-        System.out.println("Your hand totals more than 21.");
+        System.out.println("Your hand totals more than 21. You lose.");
         dealerWin();
     }catch (Exception e){
         System.out.println("There was an error!" + e);
